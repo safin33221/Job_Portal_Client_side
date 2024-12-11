c71ae362b58b9d62d889519d6e1dbd2459e722d7
safin33221 <saifn33221@gmail.com>
2024-12-11 20:47:57 +0600

    navbar section completed

---
+++ src/router/router.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layout/MainLayout';
+import Home from '../Pages/Home';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<MainLayout/>,
+        children:[
+            {
+                path:'/',
+                element:<Home/>
+            }
+        ]
+    }
+])
+
+export default router;
